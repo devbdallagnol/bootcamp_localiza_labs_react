1c8ea92a003f8eae8f560dd79ad3075ed679e89d
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-18 14:10:57 -0300

    added - rederizando elementos

--- 2_Introducao_react_js/src/index.js
+++ 2_Introducao_react_js/src/index.js
@@ -1,28 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import Button from "./components/Button";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+
+const element = "Digital Innovation"
+const element2 = <h1>Olá turma</h1>
 
 function sum(a, b) {
-  return a + b;
+  alert(a + b);
 }
 
-function primeiroJSX() {
+function App () {
   return (
-    <div className="teste">
-      <h1>Soma: {sum(10, 20)}</h1>
+    <div>
+      <h1>{element}</h1>
+      <h2>{element2}</h2>
+      <Button onClick={() => sum(10,20)} name="Bruno DallAgnol"/>
+      <ComponentA>
+        <ComponentB>
+          <Button onClick={() => sum(40,30)} name="Outro Botão"/>
+        </ComponentB>
+      </ComponentA>
     </div>
   );
 }
-
-const App = () => {
-  return (
-    <div className="App">
-      Hello, World!
-      <h2>ReactJS</h2>
-      {primeiroJSX()}
-    </div>
-  );
-};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
