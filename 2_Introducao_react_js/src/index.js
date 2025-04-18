5db44ed0e75ac8d5e5943b0720edf4e04b4a55af
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2025-04-18 13:38:31 -0300

    added - config reactjs

--- 2_Introducao_react_js/src/index.js
+++ 2_Introducao_react_js/src/index.js
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+function sum(a, b) {
+  return a + b;
+}
+
+function primeiroJSX() {
+  return (
+    <div className="teste">
+      <h1>Soma: {sum(10, 20)}</h1>
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
-      <p>This is a simple React application.</p>
+      Hello, World!
+      <h2>ReactJS</h2>
+      {primeiroJSX()}
     </div>
   );
 };
